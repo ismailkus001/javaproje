a6ccdb82f8f6e446bd06a03213249fd5b831732a
kursadhan4161 <45716553+kursadhan4161@users.noreply.github.com>
2018-12-08 19:23:34 +0300

    Add files via upload

---
+++ Makine_kontrol.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makine_kontrol;
+
+/**
+ *
+ * @author kursadhan61
+ */
+public class Makine_kontrol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
